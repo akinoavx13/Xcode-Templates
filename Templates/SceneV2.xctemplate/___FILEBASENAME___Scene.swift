934f43e847aea8a342e7c134d73063e9ce3bae6f
Maxime Maheo <imaxame@gmail.com>
2022-12-14 21:31:14 +0100

    Add scene V2

---
+++ Templates/SceneV2.xctemplate/___FILEBASENAME___Scene.swift
@@ -0,0 +1,31 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import SwiftUI
+
+struct ___VARIABLE_sceneName___Scene: View {
+    // MARK: - Properties
+
+    @StateObject var viewModel: ___VARIABLE_sceneName___SceneViewModel
+
+    // MARK: - Body
+
+    var body: some View {
+        Text("Hello, World")
+    }
+}
+
+#if DEBUG
+
+    struct ___VARIABLE_sceneName___Scene_Previews: PreviewProvider {
+        static var previews: some View {
+            ___VARIABLE_sceneName___Scene(viewModel: ___VARIABLE_sceneName___SceneViewModel.preview)
+        }
+    }
+
+#endif
---
+++ Templates/SceneV2.xctemplate/___FILEBASENAME___SceneCoordinator.swift
@@ -0,0 +1,40 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import SwiftUI
+import UIKit.UINavigationController
+
+final class ___VARIABLE_sceneName___SceneCoordinator: CoordinatorProtocol {
+    // MARK: - Properties
+
+    weak var parentCoordinator: CoordinatorProtocol?
+    var children: [CoordinatorProtocol] = []
+
+    private let navigationController: UINavigationController
+
+    // MARK: - Lifecycle
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    // MARK: - Methods
+
+    @MainActor
+    func start(params _: Any?...) {
+        let viewModel = ___VARIABLE_sceneName___SceneViewModel()
+        viewModel.coordinator = self
+
+        let scene = ___VARIABLE_sceneName___Scene(viewModel: viewModel)
+        let viewController = UIHostingController(rootView: scene)
+
+        navigationController.setViewControllers([viewController], animated: false)
+    }
+
+    func stop() { fatalError("Should not be stopped.") }
+}
