a2b1a7733e989c73e79f48292d1cec76a10efde7
akinoavx13 <imaxame@gmail.com>
2024-05-24 10:27:19 +0200

    Add TCA feature snippet

--- install.sh
+++ install.sh
@@ -2,38 +2,6 @@
 
 BASE_XCODE_DIR="${HOME}/Library/Developer/Xcode"
 
-### TEMPLATES ###
-
-TEMPLATES_XCODE_DIR="${BASE_XCODE_DIR}/Templates"
-FILE_TEMPLATES_XCODE_DIR="${TEMPLATES_XCODE_DIR}/File Templates"
-TEMPLATES_FOLDER_NAME="Templates"
-
-echo "Starting installation of templates."
-
-# Check if templates dir exists
-if [ ! -d "${TEMPLATES_XCODE_DIR}" ]; then
-  echo "Creating Templates directory, because it didn't exist."
-  mkdir "${TEMPLATES_XCODE_DIR}"
-fi
-
-# Check if file templates dir exists
-if [ ! -d "${FILE_TEMPLATES_XCODE_DIR}" ]; then
-  echo "Creating File Templates directory, because it didn't exist."
-  mkdir "${FILE_TEMPLATES_XCODE_DIR}"
-fi
-
-# Check if templates exist
-if [ -d "${FILE_TEMPLATES_XCODE_DIR}/${TEMPLATES_FOLDER_NAME}" ]; then
-  echo "Deleting previous ${TEMPLATES_FOLDER_NAME} directory."
-  rm -rf "${FILE_TEMPLATES_XCODE_DIR}/${TEMPLATES_FOLDER_NAME}"
-fi
-
-# Copy the files
-echo "Copying template files."
-cp -r "${TEMPLATES_FOLDER_NAME}" "${FILE_TEMPLATES_XCODE_DIR}/"
-echo "Templates installation done."
-
-
 ### SNIPPETS ###
 
 USER_DATA_XCODE_DIR="${BASE_XCODE_DIR}/UserData"
